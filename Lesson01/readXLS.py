e7028873bb11a2d54085b02f8d734b196bb47994
Umair Khalid <umair719@gmail.com>
2015-05-23 21:01:16 -0500

    completed solution

--- Lesson01/readXLS.py
+++ Lesson01/readXLS.py
@@ -11,6 +11,7 @@ Please see the test function for the expected return format
 
 import xlrd
 from zipfile import ZipFile
+
 datafile = "2013_ERCOT_Hourly_Load_Data.xls"
 
 
@@ -24,36 +25,49 @@ def parse_file(datafile):
     sheet = workbook.sheet_by_index(0)
 
     ### example on how you can get the data
-    #sheet_data = [[sheet.cell_value(r, col) for col in range(sheet.ncols)] for r in range(sheet.nrows)]
+    # sheet_data = [[sheet.cell_value(r, col) for col in range(sheet.ncols)] for r in range(sheet.nrows)]
 
     ### other useful methods:
-    print "\nROWS, COLUMNS, and CELLS:"
-    print "Number of rows in the sheet:", 
-    print sheet.nrows
-    print "Type of data in cell (row 3, col 2):", 
-    print sheet.cell_type(3, 2)
-    print "Value in cell (row 3, col 2):", 
-    print sheet.cell_value(3, 2)
-    print "Get a slice of values in column 3, from rows 1-3:"
-    print sheet.col_values(3, start_rowx=1, end_rowx=4)
+    # print("\nROWS, COLUMNS, and CELLS:")
+    # print("Number of rows in the sheet:", )
+    # print(sheet.nrows)
+    # print("Type of data in cell (row 3, col 2):", )
+    # print(sheet.cell_type(3, 2))
+    # print("Value in cell (row 3, col 2):", )
+    # print(sheet.cell_value(3, 2))
+    # print("Get a slice of values in column 3, from rows 1-3:")
+    # print(sheet.col_values(3, start_rowx=0, end_rowx=3))
+    #
+    # print("\nDATES:")
+    # print("Type of data in cell (row 1, col 0):", )
+    # print(sheet.cell_type(1, 0))
+    # exceltime = sheet.cell_value(1, 0)
+    # print("Time in Excel format:", )
+    # print(exceltime)
+    # print("Convert time to a Python datetime tuple, from the Excel float:", )
+    # print(xlrd.xldate_as_tuple(exceltime, 0))
 
-    print "\nDATES:"
-    print "Type of data in cell (row 1, col 0):", 
-    print sheet.cell_type(1, 0)
-    exceltime = sheet.cell_value(1, 0)
-    print "Time in Excel format:",
-    print exceltime
-    print "Convert time to a Python datetime tuple, from the Excel float:",
-    print xlrd.xldate_as_tuple(exceltime, 0)
-    
-    
+    coast_data = sheet.col_values(1, start_rowx=1, end_rowx=None)
+    cd_max = max(coast_data)
+    cd_max_index = sheet.col_values(1).index(cd_max)
+    cd_max_time_xls = sheet.cell_value(cd_max_index, 0)
+    cd_max_time = xlrd.xldate_as_tuple(cd_max_time_xls, 0)
+
+    cd_min = min(coast_data)
+    cd_min_index = sheet.col_values(1).index(cd_min)
+    cd_min_time_xls = sheet.cell_value(cd_min_index, 0)
+    cd_min_time = xlrd.xldate_as_tuple(cd_min_time_xls, 0)
+
+    cd_avg = sum(coast_data) / float(len(coast_data))
+
     data = {
-            'maxtime': (0, 0, 0, 0, 0, 0),
-            'maxvalue': 0,
-            'mintime': (0, 0, 0, 0, 0, 0),
-            'minvalue': 0,
-            'avgcoast': 0
+        'maxtime': cd_max_time,
+        'maxvalue': cd_max,
+        'mintime': cd_min_time,
+        'minvalue': cd_min,
+        'avgcoast': cd_avg
     }
+
     return data
 
 
@@ -61,8 +75,8 @@ def test():
     open_zip(datafile)
     data = parse_file(datafile)
 
-    #assert data['maxtime'] == (2013, 8, 13, 17, 0, 0)
-    #assert round(data['maxvalue'], 10) == round(18779.02551, 10)
+    assert data['maxtime'] == (2013, 8, 13, 17, 0, 0)
+    assert round(data['maxvalue'], 10) == round(18779.02551, 10)
 
 
 test()
