9c410caaf7703bb704b65fcc0971e6ceadabeefb
Umair Khalid <umair719@gmail.com>
2015-06-20 20:20:50 -0500

    Mid Solution: Testing Data Quality Quiz

--- Lesson03_Data_Quality/audit.py
+++ Lesson03_Data_Quality/audit.py
@@ -25,12 +25,10 @@ that can be found in the field. e.g.
 All the data initially is a string, so you have to do some checks on the values first.
 
 """
-import codecs
 import csv
-import json
-import pprint
+import re
 
-CITIES = 'cities.csv.csv'
+CITIES = 'cities.csv'
 
 FIELDS = ["name", "timeZone_label", "utcOffset", "homepage", "governmentType_label", "isPartOf_label", "areaCode",
           "populationTotal",
@@ -38,22 +36,50 @@ FIELDS = ["name", "timeZone_label", "utcOffset", "homepage", "governmentType_lab
           "areaLand", "areaMetro", "areaUrban"]
 
 
+def getType(value):
+    if value == "NULL" or value == '':
+        return "NoneType"
+    elif value.isdigit():
+        return "int"
+    elif isFloat(value):
+        return "float"
+    elif value[0] == "{":
+        return "list"
+    else:
+        return "str"
+
+
+def isFloat(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+
 def audit_file(filename, fields):
     fieldtypes = {}
-
-    # YOUR CODE HERE
-
-
+    with open('cities.csv') as csvfile:
+        citiesreader = csv.reader(csvfile)
+        first = True
+        for row in citiesreader:
+            if first:
+                first = False
+                FIELDS = row
+            else:
+                for col in row:
+                    type = getType(col)
+                    count = + 1
     return fieldtypes
 
 
 def test():
     fieldtypes = audit_file(CITIES, FIELDS)
 
-    pprint.pprint(fieldtypes)
+    # pprint.pprint(fieldtypes)
 
-    assert fieldtypes["areaLand"] == set([type(1.1), type([]), type(None)])
-    assert fieldtypes['areaMetro'] == set([type(1.1), type(None)])
+    # assert fieldtypes["areaLand"] == set([type(1.1), type([]), type(None)])
+    # assert fieldtypes['areaMetro'] == set([type(1.1), type(None)])
 
 
 if __name__ == "__main__":
